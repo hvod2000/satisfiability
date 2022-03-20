df5131a43b7824c0965310e497ff21d716be7ab2
Uladzislau Khamkou <kham@tuta.io>
2022-03-20 18:27:55 +0300

    move QBCNF logic into separate class

--- check_satisfiability.py
+++ check_satisfiability.py
@@ -1,30 +1,37 @@
+from dataclasses import dataclass
 from functools import cache
 from time import time
 
 
-def set_value(clauses, var, value):
-    new_clauses = set()
-    for cls in clauses:
-        if cls[0] == var or cls[0] == -var:
-            sign = (cls[0] > 0) or -1
-            old_cls = cls
-            cls = None if (sign * ((value > 0) or -1) > 0) else cls[1:]
-        if cls == ():
-            return None
-        if cls is not None:
-            new_clauses.add(cls)
-    return frozenset(new_clauses)
+@dataclass(frozen=True)
+class QBCNF:
+    quantifiers: tuple
+    clauses: frozenset
 
+    def set_value(self, var, value):
+        clauses = self.clauses
+        new_clauses = set()
+        for cls in clauses:
+            if cls[0] == var or cls[0] == -var:
+                sign = (cls[0] > 0) or -1
+                old_cls = cls
+                cls = None if (sign * ((value > 0) or -1) > 0) else cls[1:]
+            if cls == ():
+                return None
+            if cls is not None:
+                new_clauses.add(cls)
+        return QBCNF(self.quantifiers, frozenset(new_clauses))
 
-@cache
-def dp(qs, clauses, ind):
-    if clauses is None:
-        return 0
-    if len(clauses) == 0:
-        return 1
-    q = quantifiers[ind - 1]
-    values = (dp(qs, set_value(clauses, ind, v), ind + 1) for v in range(2))
-    return all(values) if q == "A" else any(values)
+    @cache
+    def dp(self, ind):
+        qs, clauses = self.quantifiers, self.clauses
+        if clauses is None:
+            return 0
+        if len(clauses) == 0:
+            return 1
+        q = quantifiers[ind - 1]
+        values = (self.set_value(ind, v).dp(ind + 1) for v in range(2))
+        return all(values) if q == "A" else any(values)
 
 
 t = time()
@@ -36,5 +43,5 @@ for i in range(m):
 clauses = frozenset(cls for cls in clauses if cls)
 print(time() - t)
 t = time()
-print(dp("AE", clauses, 1))
+print(QBCNF(quantifiers, clauses).dp(1))
 print(time() - t)
