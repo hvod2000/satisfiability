d49bb621fe843c0e2808ae0fcec90699c52ccd2f
Uladzislau Khamkou <kham@tuta.io>
2022-03-20 19:43:58 +0300

    implement alternative memory representation

--- check_satisfiability.py
+++ check_satisfiability.py
@@ -34,6 +34,46 @@ class QBCNF:
         return all(values) if q == "A" else any(values)
 
 
+def eval(quantifiers, clauses):
+    old_clauses, clauses = clauses, set()
+    for clause in old_clauses:
+        clause = set(clause)
+        for v in list(clause):
+            if v > 0 and v in clause and -v in clause:
+                break
+        else:
+            clauses.add((0, frozenset(clause)))
+    clause_tree = []
+    for var in range(len(quantifiers), 0, -1):
+        old_clauses, clauses, layer = clauses, set(), {(0, 0): 0}
+        for ref, clause in old_clauses:
+            val = int(var in clause or -(-var in clause))
+            if (ref, val) not in layer:
+                layer[(ref, val)] = len(layer)
+            clauses.add((layer[(ref, val)], frozenset(clause - {var, -var})))
+        layer = (v for i, v in sorted((i, v) for v, i in layer.items()))
+        clause_tree.append(tuple(layer))
+    clause_tree = tuple(reversed(clause_tree))
+
+    def set_value(clauses, var, value):
+        old_clauses, clauses = clauses, set()
+        for tail, cls_var in (clause_tree[var - 1][cls] for cls in old_clauses):
+            if not cls_var:
+                clauses.add(tail)
+            elif cls_var * var < 0:
+                clauses.add(tail)
+        return frozenset(clauses)
+
+    @cache
+    def dp(var, clauses):
+        if not clauses or 0 in clauses:
+            return not clauses
+        values = (dp(var + 1, set_value(clauses, var, v)) for v in (-1, 1))
+        return all(values) if quantifiers[var - 1] == "A" else any(values)
+
+    return dp(1, frozenset(range(1, len(clause_tree[-1]))))
+
+
 t = time()
 n, m = map(int, input().split())
 quantifiers = input()
